dc654fa2189286e13448d17e7e43c0216f841001
张荣侨 <zrq21@mails.tsinghua.edu.cn>
2022-04-02 12:00:28 +0800

    test yml config file

---
+++ code/ObsidianToMarkdown/Lib/FileConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObsidianToMarkdown.Lib
+{
+    public class FileConfig
+    {
+           /// <summary>
+        /// obsidian vault 文件夹路径
+        /// </summary>
+        public string VaultPath { get; set; }
+
+        //Todo: 添加文件过滤选项
+    }
+}
---
+++ code/ObsidianToMarkdown/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+using ObsidianToMarkdown.Lib;
+
+namespace ObsidianToMarkdown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string configPath = "./config.yml";
+            string configYml = System.IO.File.ReadAllText(configPath);
+            var deserializer = new DeserializerBuilder()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)  // see height_in_inches in sample yml 
+                .Build();
+
+            //yml contains a string containing your YAML
+            var fileConfig = deserializer.Deserialize<FileConfig>(configYml);
+            string? vaultPath = fileConfig.VaultPath;
+            Console.WriteLine(vaultPath);
+        }
+    }
+}
